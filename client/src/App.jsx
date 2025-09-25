3d3ddc2d78a1b975eac7b588a112fa133fe0cf86
Callum James Lodge <callumjlodge@gmail.com>
2025-09-25 15:45:39 +0100

    added initial routes to client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Router } from "react-router";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import Profile from "./components/Profile.jsx";
 export default function App() {
   return (
     <>
       <h1>App</h1>
+      <Router>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:username" element={<Profile />}>
+          <Route path="/profile/:username/todo" element={<Register />} />
+          <Route path="/profile/:username/doing" element={<Register />} />
+          <Route path="/profile/:username/done" element={<Register />} />
+        </Route>
+      </Router>
     </>
   );
 }
